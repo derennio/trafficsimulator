a1a7d5c1ed7d720da9cfbe82f5fa593f583483fb
Ennio Haibach <ennio.haibach@gmail.com>
2023-10-27 13:56:34 +0200

    Implement vehicle

--- src/main/java/dhbw/porsche/domain/Car.java
+++ src/main/java/dhbw/porsche/domain/Car.java
@@ -1,4 +1,59 @@
 package dhbw.porsche.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class Car implements IVehicle {
+    /**
+     *  the car's current velocity.
+     */
+    private float velocity;
+
+    /**
+     * The car's maximum positive acceleration.
+     */
+    private float maxAccel;
+
+    /**
+     * The car's maximum brake force.
+     */
+    private float maxBrake;
+
+    /**
+     * The car's maximum velocity.
+     */
+    private float maxVelocity;
+
+    /**
+     * The car's length.
+     */
+    private float length;
+
+    /**
+     * Index of the current street.
+     */
+    private int streetIdx;
+
+    /**
+     * position relative to street length (0.0 - 1.0).
+     */
+    private double relPosition;
+
+    /**
+     * Updates the velocity of the vehicle based on the controller's instruction.
+     */
+    @Override
+    public void updateVelocity() {
+
+    }
+
+    /**
+     * Moves the vehicle based on its velocity.
+     */
+    @Override
+    public void move() {
+
+    }
 }

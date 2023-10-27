f5da6eb197969d2ffe6fcff712abfab08dca1d51
Ennio Haibach <ennio.haibach@gmail.com>
2023-10-27 14:25:10 +0200

    Implement street service and add street data

--- src/main/java/dhbw/porsche/business/StreetService.java
+++ src/main/java/dhbw/porsche/business/StreetService.java
@@ -1,8 +1,26 @@
 package dhbw.porsche.business;
 
+import dhbw.porsche.common.Point2D;
 import dhbw.porsche.domain.IVehicle;
+import dhbw.porsche.domain.Street;
+import lombok.Getter;
 
+import java.util.List;
+
+@Getter
 public class StreetService implements IStreetService {
+    private final Street[] streets;
+    private List<IVehicle> vehicles;
+
+    public StreetService() {
+        this.streets = new Street[] {
+                new Street(13.8f, new Point2D(0, 0), new Point2D(0, 100)),
+                new Street(36.1f, new Point2D(0, 100), new Point2D(100, 100)),
+                new Street(55.6f, new Point2D(100, 100), new Point2D(200, 100)),
+                new Street(36.1f, new Point2D(200, 100), new Point2D(200, 0))
+        };
+        this.vehicles = List.of();
+    }
 
     /**
      * Adds a vehicle to the street.
@@ -11,7 +29,7 @@ public class StreetService implements IStreetService {
      */
     @Override
     public void addVehicle(IVehicle vehicle) {
-
+        this.vehicles.add(vehicle);
     }
 
     /**
@@ -21,6 +39,6 @@ public class StreetService implements IStreetService {
      */
     @Override
     public void removeVehicle(IVehicle vehicle) {
-
+        this.vehicles.remove(vehicle);
     }
 }
